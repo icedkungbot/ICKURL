21604a05ab639142e1fe551de35967e191738a57
icedkungbot <132274296+icedkungbot@users.noreply.github.com>
2023-05-07 18:15:12 +0700

    050723 1814 | @icedkung | TICKET_Clear | #Fgr34h | Connect to MongoDB

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+
+const fs = require('fs');
 const ejs = require('ejs');
 const cors = require('cors');
-const port = process.env.PORT || parseInt(process.env['PORT']);
-const fs = require('fs');
 const path = require('path');
-const dotenv = require('dotenv');
-dotenv.config();
+const bcrypt = require('bcrypt');
 const sessions = require('express-session');
 const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
-const saltRound = parseInt(process.env.SALTROUND) || parseInt(process.env['SALTROUND']);
+
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(express.static(path.join(__dirname, "/views")));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/views")));
+app.use(cookieParser());
+app.use(router)
+
+const saltRound = process.env.SALTROUND || 10;
+const port = process.env.PORT || 5001;
 const oneDay = 1000 * 60 * 60 * 24;
 const sevenDay = 1000 * 60 * 60 * 24 * 7;
-
 app.use(sessions({
-    secret: process.env.SECRET || process.env['SECRET'],
+    secret: process.env.SECRET || "URLSHORTHENER",
     saveUninitialized:true,
     cookie: { maxAge: oneDay},
     resave: false 
 }));
-app.use(cookieParser());
-app.use(router)
-
+const { User, redirectURL } = require('./mongoCommands');
 
 router.get('/', (req, res) => {
     if(req.session.access){
@@ -39,103 +43,20 @@ router.get('/', (req, res) => {
         res.render('index');
     }
 });
-
-/*
-You can replace this section with MongoDB or other database to make your data private
-*/
-const user = {
-    create:(username, password,req,res)=>{
-        //save username and password to user.json
-        fs.readFile('./user.json', 'utf-8', (err, data) => {
-            if(err){
-                console.log(err);
-            }else{
-                const userJsonFile = JSON.parse(data);
-                //check is this username is exits
-                const result = userJsonFile.find((item) => {
-                    if(item.username == username){
-                       return res.json({status:"err", msg:"Username is already exits!"});
-                    }else{
-                        const newUser = {
-                            uid: userJsonFile.length + 1,
-                            username: username,
-                            password: bcrypt.hashSync(password, saltRound)
-                        }
-                        userJsonFile.push(newUser);
-                        fs.writeFile('./user.json', JSON.stringify(userJsonFile), (err) => {
-                            if(err){
-                                console.log(err);
-                            }else{
-                                console.log('create user successfully');
-                                res.redirect("/")
-                                //for ajax response
-                                //res.json({status:"ok", msg:"User registered!"})
-                            }
-                        });
-                    }
-                })
-            }
-        })
-
-    },
-    check:async (username, hashPassword, req, res)=>{
-        //check if username and password is correct
-        fs.readFile('./user.json', 'utf-8', (err, data) => {
-            if(err){
-                console.log(err);
-            }else{
-                const userJsonFile = JSON.parse(data);
-                const result = userJsonFile.find((item) => {
-                    return item.username == username;
-                });
-                if(result){
-                    const checkPassword = bcrypt.compareSync(hashPassword, result.password);
-                    if(checkPassword){
-                        console.log("password match!!")
-                        console.log("login successful")
-                        
-                        
-                        req.session.access = true;
-                        req.session.user = username;
-                        console.log(req.session)
-                        res.redirect("/view")
-                        //for ajax response
-                        //res.json({status:"ok", msg:"Login successful"})
-                    }else{
-                        console.log("Password do not match!!!")
-                        res.redirect("/")
-                        //for ajax response
-                        //res.json({status:"err", msg:"Password incorrect!"})
-                    }
-                }else{
-                    console.log("User not found!!!")
-                    res.redirect("/")
-                    //for ajax response
-                    //res.json({status:"err", msg:"User not found!"})
-                }
-            }
-        });
-    }
-};
-/*
-You can replace this section with MongoDB or other database to make your data private
-*/
-
-router.post('/login',async (req, res) => {
-    //create express session 
-    const username = req.body.username;
-    const hashPassword = req.body.password;
-    const check = await user.check(username, hashPassword, req, res);
+  
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    User.login(username, password, req, res);
 });
 
-router.get("/register", (req, res) => {
+router.get('/register', (req, res) => {
     res.render('register');
-})
+});
 
-router.post("/createuser", (req, res) => {
-    const {username , password} = req.body;
-    user.create(username, password,req,res);
-})
+router.post('/createuser', (req, res) => {
+    const { username, password } = req.body;
+    User.create(username, password, req, res);
+});
 
 router.get("/logout", (req, res) => {
     const user = req.session.user;
@@ -154,123 +75,37 @@ const accessCheck = (req, res, next) =>{
 }
 
 router.get('/view', accessCheck, (req, res) => {
-    const owner = req.session.user;
-    fs.readFile('./redirect.json', 'utf-8', (err, data) => {
-        if(err){
-            console.log(err);
-        }else{
-            const redirectJsonFile = JSON.parse(data);
-            const result = redirectJsonFile.filter((item) => {
-                return item.owner == owner;
-            });
-            res.render('view', {urls: result, username:owner});
-        }
+    const user = req.session.user;
+    const result = redirectURL.viewByUser(user);
+    result.then((data) => {
+        res.render('view', { urls: data, username: user });
+    }).catch((err) => {
+        console.log(err);
+        res.json({ status: "err", msg: "Something went wrong!" });
     });
 });
 
-router.get('/add', accessCheck, (req, res) => {
-    res.render('add');
-})
-
-router.post('/create', accessCheck, (req, res) => {
-    const originalUrl = req.body.originalUrl;
-    const shortenUrl = req.body.shortenUrl;
-    const createAt = new Date();
-    const owner = req.session.user;
-    
-    const newUrl = {
-        original_url: originalUrl,
-        shorten_url: shortenUrl,
-        create_at: createAt,
-        owner: owner,
-        clicked:0,
-        adsTime:0
-    };
-    fs.readFile('./redirect.json', 'utf-8', (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            
-            const redirectJsonFile = JSON.parse(data);
-            //check if shortenUrl is exist do nothing
-            const result = redirectJsonFile.find((item) => {
-                return item.shorten_url == shortenUrl;
-            });
-            if(result){
-                res.redirect('/view');
-                return;
-            }
-            redirectJsonFile.push(newUrl);
-            
-            fs.writeFile('./redirect.json', JSON.stringify(redirectJsonFile), (err) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.redirect('/view');
-                }
-            });
-        }
-    });
+router.post('/add', accessCheck, (req, res) => {
+    const { originalUrl, shortenUrl } = req.body;
+    const user = req.session.user;
+    redirectURL.create(originalUrl, shortenUrl, req, res);
 });
 
 router.post('/delete', accessCheck, (req, res) => {
     const shortenUrl = req.body.shorten_url;
-    console.log(`Want to delete ${shortenUrl}`)
-    fs.readFile('./redirect.json', 'utf-8', (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            const redirectJsonFile = JSON.parse(data);
-            const result = redirectJsonFile.filter((item) => {
-                return item.shorten_url != shortenUrl;
-            });
-            fs.writeFile('./redirect.json', JSON.stringify(result), (err) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.redirect('/view');
-                }
-            });
-        }
-    });
+    redirectURL.delete(shortenUrl, req, res);
 });
 
-router.get('/url', (req, res) => {
-    const desurl = "https://google.com";
-    res.render('url', {des_url:desurl});
+router.post('/update', accessCheck, (req, res) => {
+    const { originalUrl, shortenUrl } = req.body;
+    redirectURL.update(originalUrl, shortenUrl, req, res);
 });
 
-router.get('/:shortUrl', (req, res) => {
-    const shortUrl = req.params.shortUrl;
-    fs.readFile('./redirect.json', 'utf-8', (err, data) => {
-        if(err){
-            console.log(err);
-        }else{
-            const redirectJsonFile = JSON.parse(data);
-            const result = redirectJsonFile.find((item) => {
-                if(item.shorten_url == shortUrl){
-                    item.clicked++;
-                    item.adsTime = item.clicked * 500;
-                return true;
-                }
-            });
-            if(result){
-                fs.writeFile('./redirect.json', JSON.stringify(redirectJsonFile), (err) => {
-                    if(err){
-                        console.log(err);
-                    }else{
-                        console.log('update clicked successfully');
-                    }
-                });
-                res.render('url', {des_url:result.original_url});
-            }else{
-                res.redirect('/');
-            }
-        }
-    });
+router.get('/:shortenUrl', (req, res) => {
+    const shortenUrl = req.params.shortenUrl;
+    redirectURL.redirect(shortenUrl, req, res);
 });
 
-const server = app.listen(port, ()=>{
-    const host = server.address().address;
-    console.log(`App is running on ${host == "::" ? "http://localhost" : host}:${port}`)
-})
+const server = app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+}); 
---
+++ mongoCommands.js
@@ -0,0 +1,326 @@
+const {
+    MongoClient
+} = require("mongodb");
+// Replace the uri string with your connection string.
+const uri = process.env.MONGO_URI || "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+const bcrypt = require('bcrypt');
+const saltRound = process.env.SALTROUND || 10;
+
+
+/*
+  {
+  "_id": {
+    "$oid": "645778e0727e3536dd619572"
+  },
+  "uid": 1,
+  "username": "ickdev",
+  "password": "$2b$15$kbZbIB18Zt40LtU375vWiuD4VaQzwce4MPN9LfBuspDhuSa5SYGKm"
+}
+  */
+const User = {
+    create: (username, password, req, res) => {
+        //save username and password to user.json
+        client.connect(err => {
+            const collection = client.db("account").collection("user");
+            // perform actions on the collection object
+            collection.find({
+                username: username
+            }).toArray((err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (result.length > 0) {
+                        res.json({
+                            status: "err",
+                            msg: "Username is already exits!"
+                        })
+                    } else {
+                        const newUser = {
+                            uid: result.length + 1,
+                            username: username,
+                            password: bcrypt.hashSync(password, saltRound)
+                        }
+                        collection.insertOne(newUser, (err, result) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log('create user successfully');
+                                res.redirect("/")
+                                //for ajax response
+                                //res.json({status:"ok", msg:"User registered!"})
+                            }
+                        })
+                    }
+                }
+            });
+            client.close();
+        });
+    },
+    login: async (username, password, req, res) => {
+        client.connect(err => {
+            const collection = client.db("account").collection("user");
+            // perform actions on the collection object
+            collection.find({
+                username: username
+            }).toArray((err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (result.length == 0) {
+                        res.json({
+                            status: "err",
+                            msg: "Username is not exits!"
+                        })
+                    } else {
+                        const checkPassword = bcrypt.compareSync(password, result[0].password);
+                        if (checkPassword) {
+                            console.log("password match!!")
+                        console.log("login successful")
+                        
+                        
+                        req.session.access = true;
+                        req.session.user = username;
+                        console.log(req.session)
+                        res.redirect("/view")
+                        } else {
+                            console.log("Password incorrect!")
+                        res.redirect("/")
+                        }
+                    }
+                }
+            })
+            client.close();
+        });
+    }
+}
+
+/*
+{
+  "_id": {
+    "$oid": "64577944727e3536dd619579"
+  },
+  "original_url": "https://www.youtube.com/watch?v=TcLz34_G0u8",
+  "shorten_url": "yt",
+  "create_at": "2023-05-05T19:00:32.568Z",
+  owner: owner,
+  clicked:0,
+  adsTime:0
+}
+*/
+const redirectURL = {
+        viewALL: () => {
+            //get all shorten url 
+            client.connect(err => {
+                const collection = client.db("account").collection("redirect");
+                // perform actions on the collection object
+                collection.find({}).toArray((err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        return result;
+                    }
+                })
+                client.close();
+            });
+        },
+        viewByUser: (username) => {
+            //get all shorten url by username
+            client.connect(err => {
+                const collection = client.db("account").collection("redirect");
+                // perform actions on the collection object
+                collection.find({
+                    owner: username
+                }).toArray((err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        return result;
+                    }
+                })
+                client.close();
+            });
+        },
+        viewByShorten: (shortenUrl) => {
+            //get all shorten url by shorten url
+            client.connect(err => {
+                const collection = client.db("account").collection("redirect");
+                // perform actions on the collection object
+                collection.find({
+                    shorten_url: shortenUrl
+                }).toArray((err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        return result;
+                    }
+                })
+                client.close();
+            });
+        },
+        viewByOriginal: (originalUrl) => {
+            //get all shorten url by original url
+            client.connect(err => {
+                const collection = client.db("account").collection("redirect");
+                // perform actions on the collection object
+                collection.find({
+                    original_url: originalUrl
+                }).toArray((err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        return result;
+                    }
+                })
+                client.close();
+            });
+        },
+        create: (originalUrl, shortenUrl, req, res) => {
+            //save shorten url to shorten.json
+
+            client.connect(err => {
+                const collection = client.db("account").collection("redirect");
+                // perform actions on the collection object
+                collection.find({
+                    shorten_url: shortenUrl
+                }).toArray((err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        if (result.length > 0) {
+                            res.json({
+                                status: "err",
+                                msg: "Shorten URL is already exits!"
+                            })
+                        } else {
+                            const newShortenUrl = {
+                                id: result.length + 1 + owner,
+                                original_url: originalUrl,
+                                shorten_url: shortenUrl,
+                                create_at: new Date(),
+                                owner: owner,
+                                clicked: 0,
+                                adsTime: 0
+                            }
+
+                            collection.insertOne(newShortenUrl, (err, result) => {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    console.log('create shorten url successfully');
+                                    res.redirect("/view")
+                                    //for ajax response
+                                    //res.json({status:"ok", msg:"Shorten URL created!"})
+                                }
+                            })
+                        }
+                    }
+                });
+                client.close();
+            });
+        },
+        delete: (shortenUrl, req, res) => {
+            //delete shorten url from shorten.json
+            const owner = req.session.user;
+            client.connect(err => {
+                const collection = client.db("account").collection("redirect");
+                // perform actions on the collection object
+                collection.deleteOne({
+                    shorten_url: shortenUrl,
+                    owner: owner
+                }, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('delete shorten url successfully');
+                        res.redirect("/view")
+                        //for ajax response
+                        //res.json({status:"ok", msg:"Shorten URL deleted!"})
+                    }
+                })
+                client.close();
+            });
+
+        },
+        update: (shortenUrl, newShortenUrl, req, res) => {
+            //update shorten url from shorten.json
+            const owner = req.session.user;
+            client.connect(err => {
+                const collection = client.db("account").collection("redirect");
+                // perform actions on the collection object
+                collection.updateOne({
+                    shorten_url: shortenUrl,
+                    owner: owner
+                }, {
+                    $set: {
+                        shorten_url: newShortenUrl
+                    }
+                }, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('update shorten url successfully');
+                        res.redirect("/view")
+                        //for ajax response
+                        //res.json({status:"ok", msg:"Shorten URL updated!"})
+                    }
+                })
+                client.close();
+            });
+        },
+        redirect: (shortenUrl, username, req, res) => {
+                const collection = client.db("account").collection("redirect");
+                // perform actions on the collection object
+                collection.find({
+                        shorten_url: shortenUrl
+                    }).toArray((err, result) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                result.clicked += 1;
+                                result.adsTime = result.clicked * 5;
+                                collection.updateOne({
+                                    shorten_url: shortenUrl
+                                }, {
+                                    $set: {
+                                        clicked: result.clicked,
+                                        adsTime: result.adsTime
+                                    },
+                                }, (err, result) => {
+                                    if (err) {
+                                        console.log(err);
+                                    } else {
+                                        console.log('update shorten url successfully');
+                                        res.render('url', {des_url:result.original_url});
+                                        //for ajax response
+                                        //res.json({status:"ok", msg:"Shorten URL updated!"})
+                                    }
+                                });
+                            }
+                        })
+                        client.close();
+            },
+            view: (shortenUrl, username, req, res) => {
+                const collection = client.db("account").collection("redirect");
+                const owner = username;
+                // perform actions on the collection object
+                collection.find({
+                    shorten_url: shortenUrl,
+                    owner: owner
+                }).toArray((err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        return result;
+                    }
+                })
+                client.close();
+            }
+}
+module.exports = {User, redirectURL};
