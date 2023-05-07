32a2c70459ba44ba9b307d3424a663bd7e78525b
icedkungbot <132274296+icedkungbot@users.noreply.github.com>
2023-05-07 18:36:09 +0700

    050723 1835 | @icedkung | TICKET_Clear | #Fgr34x | FIX Mongo API Version

--- mongoCommands.js
+++ mongoCommands.js
@@ -3,7 +3,7 @@ const {
 } = require("mongodb");
 // Replace the uri string with your connection string.
 const uri = process.env.MONGO_URI || "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
-const client = MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const bcrypt = require('bcrypt');
 const saltRound = process.env.SALTROUND || 10;
 
